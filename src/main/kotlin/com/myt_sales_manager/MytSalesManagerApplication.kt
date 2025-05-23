77bdbb66cd723e21526e9c98b8a428f8710c12ca
prsNatan <npires@uolinc.com>
2025-05-23 14:40:00 -0300

    initial config

---
+++ src/main/kotlin/com/myt_sales_manager/MytSalesManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.myt_sales_manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MytSalesManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MytSalesManagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/myt_sales_manager/MytSalesManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.myt_sales_manager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MytSalesManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
